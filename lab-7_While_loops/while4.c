9ad4b99acc5810449b41bad44c79407d400fb719
ketanmakwana30 <ketanmakwana0130@gmail.com>
2025-08-26 22:00:55 +0530

    add

--- lab-7_While_loops/while4.c
+++ lab-7_While_loops/while4.c
@@ -1,4 +1,4 @@
-// print odd numbers from N to 1 using a while loop.
+    // print odd numbers from N to 1 using a while loop.
 
 // if (n % 2 != 0) condition ensures only odd numbers are printed.
 
@@ -9,7 +9,7 @@ int main() {
     
     printf("Enter the value of N: ");
     scanf("%d", &n);
-    
+        
     while (n >= 1) {
         if (n % 2 != 0) {
             printf("%d\n", n);
